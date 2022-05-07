df1fe853fc9daa9959e9ca7b3c3be31146fa16ff
tywalch <tywalch@gmail.com>
2022-05-07 11:12:47 -0400

    autocurtains for devices that dont support hover

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,15 @@
-console.log('hello');
+(function WalchPizza() {
+  const cannotHover = matchMedia('(hover: none)').matches;
+  console.log({cannotHover});
+  if (!cannotHover) {
+    setTimeout(function() {
+      const elements = document.getElementsByClassName('rnOuter');
+      for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        console.log('before', {element});
+        element.classList.add('opened');
+        console.log('after', {element});
+      }
+    }, 6000);
+  }
+}());    
